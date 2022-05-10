b01fc455e8e5e54765829f6b81dde4b8ae5a7f5a
Purti Mishra <91051576+PurtiMishra@users.noreply.github.com>
2022-05-10 19:14:58 +0530

    Time: 23 ms (39.97%), Space: 5.9 MB (34.18%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -4,7 +4,7 @@ public:
     bool isPalindrome(int x) {
         if( x < 0 || x >= 2147483647)
             return false;
-        long int num = 0;
+         long int num = 0;
         int y = x;
         while(x > 0){
             num = num * 10;
