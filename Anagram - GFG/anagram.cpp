fdef0c42e8690655266ea31eceacd630046832cf
Purti Mishra <91051576+PurtiMishra@users.noreply.github.com>
2022-12-20 23:12:00 +0530

    Update anagram.cpp

--- Anagram - GFG/anagram.cpp
+++ Anagram - GFG/anagram.cpp
@@ -13,11 +13,11 @@ class Solution
         int count[26]={0};
      if(a.size()!= b.size()) return false;
       for(int i=0; i<a.size(); i++){
-	        count[a[i]-'a']++;
-	        count[b[i]-'a']--;
+	        count[a[i]-'a']++;//here we are increasing the count of each alpahbet in the count array by 1
+	        count[b[i]-'a']--;//here we are decresing the count of each alphabet in the same count array by 1
 	    }
 	  for(int i=0; i<26; i++){
-	        if(count[i]>0){
+	        if(count[i]>0){//if our strings where anagram then the count array for each charecter will store 0 value..if not then it is not anagram so return false
 	           return false;
 	        }
 	    }
